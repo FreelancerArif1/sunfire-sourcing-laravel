a738174ef7957c7f2803e89325894b62bf919cf2
FreelancerArif1 <58873534+FreelancerArif1@users.noreply.github.com>
2026-03-04 06:06:15 +0600

    Update sidebar.blade.php

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard" style="background-color: black;">
       <div class="sidebar-brand-icon">
-        <img src="{{asset($wss->logo ?? '')}}" height="60px" alt="">
+        <img src="{{asset($wss->logo ?? '')}}" alt="">
       </div>
       {{-- <div class="sidebar-brand-text mx-3">Sunfire Sourcing</div> --}}
     </a>
